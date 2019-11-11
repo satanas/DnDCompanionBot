1fc21c18c5c8161ffb3e0f7ca4c38e3ca78837dd
satanas <wil.alejandro@gmail.com>
2019-11-11 00:36:02 -0800

    Testing command handler approach

--- main.py
+++ main.py
@@ -14,13 +14,43 @@ from campaign import handler as campaign_handler
 from telegram.ext import Updater
 from telegram.ext import CommandHandler
 
+# cmd, handler, args, description
+GENERAL_COMMANDS = {
+    "/start": (start, None, "starts the DnDCompanionBot"),
+    "/roll": (roll_handler, "<expression>", "rolls the dice using the [dice notation](https://en.wikipedia.org/wiki/Dice_notation)"),
+    "/charsheet": (charsheet_handler, "<username>", "returns the character sheet associated with username"),
+    #"/combatsheet": (None, None, "returns the link of the combat cheatsheet"),
+    "/help": (help_handler, None, "shows this help message"),
+}
+
+CAMPAIGN_COMMANDS = {
+    "/start_campaign": (campaign_handler, None, "starts a new campaign in the invoked group"),
+    "/close_campaign": (campaign_handler, None, "closes an active campaign"),
+    "/set_turns": (turn_handler, "<username1>,...<usernameN>", "creates a list with the order of players for a given round"),
+    "/turn": (turn_handler, None, "shows the current player in the turns list"),
+    "/next_turn": (turn_handler,  None, "moves to the next player in the turns list"),
+    "/set_dm": (dm_handler, "<username>", "sets the username of the DM for the current campaign"),
+    "/dm": (dm_handler, None, "shows the DM for the current campaign"),
+}
+
+CHARACTER_COMMANDS = {
+    "/import_char": (character_handler, "<url>", "imports the JSON data of a character from a URL"),
+    "/link_char": (character_handler, "<char_id> <username> OR <char_id>", "links character to target username or self username"),
+    "/weapons": (character_handler, "<character>", "shows the list of weapons of a character"),
+    "/attack_roll": (character_handler, "<character> <weapon> melee|range <distance> adv|disadv", "performs an attack roll on a character"),
+    "/initiative_roll": (character_handler, "<character>", "performs an initiative roll for a character"),
+    "/talk": (character_handler, "<character> <message>", "prints a message using in-game conversation format"),
+    "/say": (character_handler, "<character> <message>", "prints a normal message using in-game conversation format"),
+    "/whisper": (character_handler, "<character> <message>", "prints a whisper message using in-game conversation format"),
+    "/yell": (character_handler, "<character> <message>", "prints a yell message using in-game conversation format"),
+}
+
 logger = logging.getLogger()
 if logger.handlers:
     for handler in logger.handlers:
         logger.removeHandler(handler)
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                      level=logging.INFO)
-
 
 OK_RESPONSE = {
     'statusCode': 200,
@@ -31,7 +61,6 @@ ERROR_RESPONSE = {
     'statusCode': 500,
     'body': json.dumps('Oops, something went wrong!')
 }
-
 
 def configure_telegram():
     """
@@ -57,32 +86,49 @@ def webhook(event, context):
 
     if event.get('httpMethod') == 'POST' and event.get('body'):
         update = telegram.Update.de_json(json.loads(event.get('body')), bot)
-        if update.message == None or update.message.text == None:
+        if update.message == None or update.message.text == None or update.message.text == '':
             return OK_RESPONSE
 
-        text = update.message.text
+        command = update.message.text.split(' ')[0]
+        instruction = update.message.text
 
-        if text == '/start':
-            start(bot, update)
-        elif text.startswith('/roll'):
-            roll_handler(bot, update)
-        elif text.startswith('/charsheet'):
-            charsheet_handler(bot, update)
-        elif text.startswith('/help'):
-            help_handler(bot, update)
-        elif text.startswith('/import_char') or text.startswith('/attack_roll') or \
-                text.startswith('/initiative_roll') or text.startswith('/weapons') or \
-                text.startswith('/talk') or text.startswith('/say') or \
-                text.startswith('/whisper') or text.startswith('/yell'):
-            character_handler(bot, update)
-        elif text.find('turn') > 0:
-            turn_handler(bot, update)
-        elif text.startswith('/set_dm') or text.startswith('/dm'):
-            dm_handler(bot, update)
-        elif text.find('campaign') > 0:
-            campaign_handler(bot, update)
+        handler = None
+        if command in GENERAL_COMMANDS:
+            handler = GENERAL_COMMANDS[command][0]
+        elif command in CAMPAIGN_COMMANDS:
+            handler = CAMPAIGN_COMMANDS[command][0]
+        elif command in CHARACTER_COMMANDS:
+            handler = CHARACTER_COMMANDS[command][0]
 
-        return OK_RESPONSE
+        if handler != None:
+            handler(instruction)
+            return OK_RESPONSE
+        else:
+            return ERROR_RESPONSE
+
+        #text = update.message.text
+
+        #if text == '/start':
+        #    start(bot, update)
+        #elif text.startswith('/roll'):
+        #    roll_handler(bot, update)
+        #elif text.startswith('/charsheet'):
+        #    charsheet_handler(bot, update)
+        #elif text.startswith('/help'):
+        #    help_handler(bot, update)
+        #elif text.startswith('/import_char') or text.startswith('/attack_roll') or \
+        #        text.startswith('/initiative_roll') or text.startswith('/weapons') or \
+        #        text.startswith('/talk') or text.startswith('/say') or \
+        #        text.startswith('/whisper') or text.startswith('/yell'):
+        #    character_handler(bot, update)
+        #elif text.find('turn') > 0:
+        #    turn_handler(bot, update)
+        #elif text.startswith('/set_dm') or text.startswith('/dm'):
+        #    dm_handler(bot, update)
+        #elif text.find('campaign') > 0:
+        #    campaign_handler(bot, update)
+
+        #return OK_RESPONSE
 
     return ERROR_RESPONSE
 
